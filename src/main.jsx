6d219249399beed21fa55ee6f4062be20daa8ceb
AWB TRAVELS AND TOURS <awbtravelsntours@outlook.com>
2025-07-01 01:34:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import "./index.css"; // Tailwind CSS
+import "./index.css";
 
 // Page components
 import HomePage from "./pages/HomePage";
@@ -19,8 +19,8 @@ import NewsPage from "./pages/NewsPage";
 import TestimonialsPage from "./pages/TestimonialsPage";
 import Cars from "./pages/Cars";
 import Properties from "./pages/Properties";
-import ToursPage from "./pages/ToursPage"; // Tours main page
-import TourResults from "./pages/TourResults"; // ✅ NEW: Tour results by category
+import ToursPage from "./pages/ToursPage";
+import TourResults from "./pages/TourResults"; // Tour category results page
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -31,7 +31,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="consultation" element={<ConsultationPage />} />
           <Route path="consultation-success" element={<ConsultationSuccess />} />
           <Route path="tours" element={<ToursPage />} />
-          <Route path="tours/category/:categoryId" element={<TourResults />} /> {/* ✅ NEW dynamic tour results */}
+          <Route path="tours/category/:categoryId" element={<TourResults />} />
           <Route path="flights" element={<Flights />} />
           <Route path="hotels" element={<Hotels />} />
           <Route path="insurance" element={<Insurance />} />
