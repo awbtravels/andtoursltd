77eee770be8a6bc606aee3e3f2edd8700e0f8710
AWB TRAVELS AND TOURS <awbtravelsntours@outlook.com>
2025-07-01 02:13:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./index.css";
 
-// Page components
 import HomePage from "./pages/HomePage";
 import ConsultationPage from "./pages/ConsultationPage";
 import ConsultationSuccess from "./pages/ConsultationSuccess";
@@ -19,7 +19,7 @@ import TestimonialsPage from "./pages/TestimonialsPage";
 import Cars from "./pages/Cars";
 import Properties from "./pages/Properties";
 import ToursPage from "./pages/ToursPage";
-import TourResults from "./pages/TourResults"; // ✅ Tour results page
+import TourResults from "./pages/TourResults";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
