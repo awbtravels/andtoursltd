766af255801801561cdfa10adef3c50c6e2d78ae
AWB TRAVELS AND TOURS <awbtravelsntours@outlook.com>
2026-02-13 01:00:28 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,7 +47,6 @@ export default function Footer() {
             📞 +2348026028245<br />
             📞 +2348102985914<br />
             ✉ info@awbtravelsandtours.com<br />
-          
             ✉ customerservice@awbtravelsandtours.com
           </p>
         </div>
